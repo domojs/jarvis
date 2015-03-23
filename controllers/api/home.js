c82c172f23fa16810a2c546062334a1c928ec170
npenin <nicolas.penin@dragon-angel.fr>
2015-03-23 07:33:21 +0100

    added package.json + improvements

--- controllers/api/home.js
+++ controllers/api/home.js
@@ -6,24 +6,18 @@ module.exports={
             console.log(arguments);
 			callback(a);
 		});
-		return;
-		socket=$('net').connect(5001, function(){
-			socket.setEncoding('utf8');
+	},
+	rules:function(callback)
+	{
+        chat.send('rules');
+		chat.once('message', function(a){
+            console.log(arguments);
+			callback(a.rules);
 		});
-		socket.on('data', function(data){
-			if(data=='>')
-			{
-				console.log(data+q);
-				socket.write(q);
-				return;
-			}
-			console.log(data+q);
-			callback(data);
-			socket.end();
-		})
-		.on('error', function(err){
-			console.log(err);
-			callback(500);
-		});
+
+	},
+	lessons:function(callback)
+	{
+        callback(Object.keys($('./modules/chat/lessons.json')));
 	}
 };
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 exports.init=function(config){
-    global.chat=$('child_process').fork('./server.js', [], {cwd:'./modules/chat/', silent:true});
+    global.chat=$('child_process').fork('./server.js', [], 
+{cwd:'./modules/chat/', silent:true});
 	chat.stdin.setEncoding('utf8');
 	chat.stdout.setEncoding('utf8');
+	chat.stderr.setEncoding('utf8');
     chat.stdout.on('data', function(message){
         $.emit('message', message);
     });
+    chat.stderr.on('data', function(message){
+        console.log(message);
+    });
     chat.on('message', function(message){
-        $.emit('message', message);
+        $.emit('message', message );
     });
 };
